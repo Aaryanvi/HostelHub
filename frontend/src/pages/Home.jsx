a91a51647724c391592f9ad8a2fc7601932ccd28
Aaryanvi <24cs10aa2@mitsgwl.ac.in>
2026-08-08 21:34:39 +0530

    setup frontend and routing

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>HostelHub</h1>
+            <p>Campus marketplace for students.</p>
+        </div>
+    );
+}
+
+export default Home;
